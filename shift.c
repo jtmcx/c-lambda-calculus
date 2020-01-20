ac4cf8cb17f500490f54051a77b54c844cdbe388
jtm <noreply@jtm.cx>
2020-01-20 18:29:27 -0500

    Refactored shift.c

--- shift.c
+++ shift.c
@@ -1,5 +1,37 @@
 #include "all.h"
 
+static Term* shiftvar(Term *t, int cutoff, int amt);
+static Term* shiftlam(Term *t, int cutoff, int amt);
+static Term* shiftapp(Term *t, int cutoff, int amt);
+
+Term*
+shift(Term *t, int cutoff, int amt)
+{
+	assert(t);
+	switch (t->tag) {
+	case TERM_VAR:
+		return shiftvar(t, cutoff, amt);
+	case TERM_LAM:
+		return shiftlam(t, cutoff, amt);
+	case TERM_APP:
+		return shiftapp(t, cutoff, amt);
+	}
+	assert(!"unreachable");
+	return NULL;
+}
+
+Term*
+shiftup(Term *t)
+{
+	return shift(t, 0, 1);
+}
+
+Term*
+shiftdown(Term *t)
+{
+	return shift(t, 0, -1);
+}
+
 static Term*
 shiftvar(Term *t, int cutoff, int amt)
 {
@@ -29,32 +61,4 @@ shiftapp(Term *t, int cutoff, int amt)
 	lhs = shift(t->u.app.lhs, cutoff, amt);
 	rhs = shift(t->u.app.rhs, cutoff, amt);
 	return mkapp(lhs, rhs);
-}
-
-Term*
-shift(Term *t, int cutoff, int amt)
-{
-	assert(t);
-	switch (t->tag) {
-	case TERM_VAR:
-		return shiftvar(t, cutoff, amt);
-	case TERM_LAM:
-		return shiftlam(t, cutoff, amt);
-	case TERM_APP:
-		return shiftapp(t, cutoff, amt);
-	}
-	assert(!"unreachable");
-	return NULL;
-}
-
-Term*
-shiftup(Term *t)
-{
-	return shift(t, 0, 1);
-}
-
-Term*
-shiftdown(Term *t)
-{
-	return shift(t, 0, -1);
 }
