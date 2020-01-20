bbead0ce4ac3680e563a5a04f32ce3d0066f8875
jtm <noreply@jtm.cx>
2020-01-20 18:27:45 -0500

    Refactored reduce.c

--- reduce.c
+++ reduce.c
@@ -1,34 +1,7 @@
 #include "all.h"
 
-static Term*
-reducelam(Term *t)
-{
-	assert(t);
-	assert(t->tag == TERM_LAM);
-	return mklam(reduce(t->u.lam));
-}
-
-static Term*
-reduceapp(Term *t)
-{
-	Term *lhs, *rhs;
-	assert(t);
-	assert(t->tag == TERM_APP);
-	rhs = t->u.app.rhs;
-	lhs = reduce(t->u.app.lhs);
-	if (lhs->tag == TERM_LAM) {
-		/* Beta reduce the application. */
-		t = shiftdown(subst(lhs->u.lam, 0, shiftup(rhs)));
-
-		/* Further reduce the expression (this will recurse until
-		 * we have a "head normal form" for this expression). */
-		return reduce(t);
-	}
-
-	/* If get here, the left hand side is in head normal form. We
-	 * then further reduce the right hand side of the expression. */
-	return mkapp(lhs, reduce(rhs));
-}
+static Term* reducelam(Term *t);
+static Term* reduceapp(Term *t);
 
 Term*
 reduce(Term *t)
@@ -44,4 +17,37 @@ reduce(Term *t)
 	}
 	assert(!"unreachable");
 	return NULL;
+}
+
+static Term*
+reducelam(Term *t)
+{
+	assert(t);
+	assert(t->tag == TERM_LAM);
+	return mklam(reduce(t->u.lam));
+}
+
+static Term*
+reduceapp(Term *t)
+{
+	Term *lhs;
+
+	assert(t);
+	assert(t->tag == TERM_APP);
+
+	/* Start by reducing terms on the left hand side. */
+	lhs = reduce(t->u.app.lhs);
+
+	if (lhs->tag == TERM_LAM) {
+		/* Beta reduce the application. */
+		t = shiftdown(subst(lhs->u.lam, 0, shiftup(t->u.app.rhs)));
+
+		/* Further reduce the expression (this will recurse until
+		 * we have a "head normal form" for this expression). */
+		return reduce(t);
+	}
+
+	/* If get here, the left hand side is in head normal form. We
+	 * then further reduce the right hand side of the expression. */
+	return mkapp(lhs, reduce(t->u.app.rhs));
 }
