8812774f97822846c12da48064d2a00be80cafb4
jtm <noreply@jtm.cx>
2020-01-20 18:24:34 -0500

    Removed print of input term

--- main.c
+++ main.c
@@ -6,9 +6,6 @@ main(int argc, char *argv[])
 	Term *t = parse(stdin);
 	if (t == NULL)
 		return 1;
-
-	dump(stdout, t);
-	fprintf(stdout, "\n");
 	dump(stdout, reduce(t));
 	fprintf(stdout, "\n");
 	return 0;
