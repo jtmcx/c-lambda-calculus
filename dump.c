53b49fe520122d493cd8a0390ac4ecfd26e4f15b
jtm <noreply@jtm.cx>
2020-01-20 18:14:01 -0500

    Cleaned up dump.c, added ability to print deeply nested lambdas

--- dump.c
+++ dump.c
@@ -1,48 +1,16 @@
 #include "all.h"
 
-static char letters[] = "abcdefghijklmnopqrstuvwxyz";
-
 static void dodump(FILE *f, Term *t, int depth);
+static void ident(FILE *f, int index);
+static void parens(FILE *f, Term *t, int depth);
+static void dumpvar(FILE *f, Term *t, int depth);
+static void dumplam(FILE *f, Term *t, int depth);
+static void dumpapp(FILE *f, Term *t, int depth);
 
-static void
-dumpvar(FILE *f, Term *t, int depth)
+void
+dump(FILE *f, Term *t)
 {
-	assert(t);
-	assert(t->tag == TERM_VAR);
-	assert(t->u.var < 26);       /* This is really dumb. */
-	fprintf(f, "%c", letters[depth - t->u.var - 1]);
-}
-
-static void
-dumplam(FILE *f, Term *t, int depth)
-{
-	assert(t);
-	assert(t->tag == TERM_LAM);
-	assert(depth < 26);       /* This is also really dumb. */
-	fprintf(f, "\\%c. ", letters[depth]);
-	dodump(f, t->u.lam, depth+1);
-}
-
-static void
-dumpapp(FILE *f, Term *t, int depth)
-{
-	assert(t);
-	assert(t->tag == TERM_APP);
-	if (t->u.app.lhs->tag != TERM_VAR) {
-		fprintf(f, "(");
-		dodump(f, t->u.app.lhs, depth);
-		fprintf(f, ")");
-	} else {
-		dodump(f, t->u.app.lhs, depth);
-	}
-	fprintf(f, " ");
-	if (t->u.app.rhs->tag == TERM_APP) {
-		fprintf(f, "(");
-		dodump(f, t->u.app.rhs, depth);
-		fprintf(f, ")");
-	} else {
-		dodump(f, t->u.app.rhs, depth);
-	}
+	dodump(f, t, 0);
 }
 
 static void
@@ -62,8 +30,69 @@ dodump(FILE *f, Term *t, int depth)
 	}
 }
 
-void
-dump(FILE *f, Term *t)
+static void
+ident(FILE *f, int index)
 {
-	dodump(f, t, 0);
+	/* Print out a unique identifier for some De Bruijn index.
+	Follows the pattern: a, b, c, ... aa, bb, cc, ... */
+
+	static char letters[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int c = letters[index % 26];
+	do {
+		fprintf(f, "%c", c);
+	} while ((index -= 26) >= 0);
+}
+
+static void
+parens(FILE *f, Term *t, int depth)
+{
+	fprintf(f, "(");
+	dodump(f, t, depth);
+	fprintf(f, ")");
+}
+
+static void
+dumpvar(FILE *f, Term *t, int depth)
+{
+	assert(t);
+	assert(t->tag == TERM_VAR);
+	ident(f, depth - t->u.var - 1);
+}
+
+static void
+dumplam(FILE *f, Term *t, int depth)
+{
+	assert(t);
+	assert(t->tag == TERM_LAM);
+	fprintf(f, "\\");
+	ident(f, depth);
+	fprintf(f, ". ");
+	dodump(f, t->u.lam, depth + 1);
+}
+
+static void
+dumpapp(FILE *f, Term *t, int depth)
+{
+	assert(t);
+	assert(t->tag == TERM_APP);
+
+	/* Lambda's need to be parenthesized if they are on 
+	the left hand side of an application. */
+	if (t->u.app.lhs->tag == TERM_LAM) {
+		parens(f, t->u.app.lhs, depth);
+	} else {
+		dodump(f, t->u.app.lhs, depth);
+	}
+
+	fprintf(f, " ");
+
+	/* Application is left associative, so if there is an
+	application on the right side, we need to parenthesize. 
+	Lambda's are also easier to read if parenthesized. */
+	if (t->u.app.rhs->tag != TERM_VAR) {
+		parens(f, t->u.app.rhs, depth);
+	} else {
+		dodump(f, t->u.app.rhs, depth);
+	}
 }
